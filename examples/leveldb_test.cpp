cb18196a64a0b2013a9bc6ea1bcdaf8e9b97faed
yaojun <940334249@qq.com>
2024-02-25 19:39:47 +0800

    add examples

--- examples/leveldb_test.cpp
+++ examples/leveldb_test.cpp
@@ -8,17 +8,21 @@ int main()
 {
     leveldb::DB *db;
     leveldb::Options options;
+    // 当数据库不存在时，是否创建一个新的数据库
     options.create_if_missing = true;
     leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
     assert(status.ok());
-    std::string k1 = "name";
-    std::string v1 = "jim";
-    status = db->Put(leveldb::WriteOptions(), k1, v1);
+    // Slice是封装后的string
+    // Slice转换为string: s.ToString()
+    // string转换为Slice: leveldb::Slice key1(mystring)
+    leveldb::Slice key1("key1"), value1("value1");
+    std::string value2;
+    status = db->Put(leveldb::WriteOptions(), key1, value1);
     assert(status.ok());
 
-    status = db->Get(leveldb::ReadOptions(), k1, &v1);
+    status = db->Get(leveldb::ReadOptions(), key1, &value2);
     assert(status.ok());
-    std::cout << "k1:" << k1 << "; v1:" << v1 << std::endl;
+    std::cout << "key1: " << key1.ToString() << "; value2:" << value2 << std::endl;
     delete db;
     return 0;
 }
